6f9a0e6994bd3bd30e3c39acb6dc6ccff6ccc151
Nassim <Nassim.Touissi.Etu@univ-lemans.fr>
2025-12-04 11:01:57 +0100

    Creation PSQL db and upload init.sql file

--- core/models.py
+++ core/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+import uuid # To have unique ID and not 1234...
 
-# Create your models here.
+"""
+class MediaFile
+    - Settings : models.Model
+    - Goal : Stock media informations in PSQL Table
+"""
+class MediaFile(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    file_name = models.TextField()
+    file_type = models.CharField(
+        max_length=50,
+        choices=[("image", "Image"), ("video", "Video"), ("other", "Other")],
+        default="other"
+    )
+    url = models.URLField()
+    size = models.BigIntegerField(unique=True, primary_key=False, serialize=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    meta_data = models.BooleanField()
---
+++ init.sql
@@ -0,0 +1,27 @@
+-- Create Database
+CREATE DATABASE media ENCODING = 'UTF8';
+
+-- Create User dragibus
+CREATE USER media WITH ENCRYPTED PASSWORD 'media';
+
+GRANT CONNECT ON DATABASE media TO media;
+GRANT USAGE ON SCHEMA public TO media;
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO media;
+GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO media;
+
+\connect music
+
+--Creation
+CREATE SCHEMA IF NOT EXISTS "media";
+
+GRANT ALL PRIVILEGES ON SCHEMA "media" TO media;
+GRANT USAGE ON SCHEMA "media" TO media;
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA "media" TO media;
+GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA "media" TO media;
+
+--set default schema
+ALTER USER media SET search_path = "media";
+
+SET search_path TO "media";
+
+CREATE OR REPLACE LANGUAGE plpgsql;
